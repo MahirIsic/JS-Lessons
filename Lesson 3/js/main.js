5da50cc8f7edd67618b47ab68c64e0a923c71fe1
Isko4859 <mahir.isic54@gmail.com>
2025-10-15 10:34:18 -0400

    Lessons

---
+++ Lesson 3/js/main.js
@@ -0,0 +1,18 @@
+const myVariable = "Mathematics";
+
+console.log("Every good boy does fine".length);
+
+console.log(myVariable.charAt(0));
+
+console.log(myVariable.indexOf("at"));
+
+console.log(myVariable.lastIndexOf("at"));
+
+console.log(myVariable.slice(2, 5));
+
+console.log(myVariable.toUpperCase());
+
+console.log(myVariable.includes("mat"))
+
+console.log(myVariable.split("e"))
+
